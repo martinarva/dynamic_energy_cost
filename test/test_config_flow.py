a53b94a1c3c1e6b5286b12166bd725d414dbed8a
BDVGitHub <139714739+BDVGitHub@users.noreply.github.com>
2025-05-12 10:44:57 +0000

    style fixes by ruff

--- test/test_config_flow.py
+++ test/test_config_flow.py
@@ -5,17 +5,21 @@ import pytest
 from homeassistant import config_entries, setup
 from custom_components.dynamic_energy_cost.const import DOMAIN
 
+
 @pytest.mark.asyncio
 async def test_form(hass):
     """Test we get the form."""
     # Ensure the component is set up correctly
     await setup.async_setup_component(hass, "persistent_notification", {})
 
-    with patch(
-        "custom_components.dynamic_energy_cost.async_setup", return_value=True
-    ) as mock_setup, patch(
-        "custom_components.dynamic_energy_cost.async_setup_entry", return_value=True
-    ) as mock_setup_entry:
+    with (
+        patch(
+            "custom_components.dynamic_energy_cost.async_setup", return_value=True
+        ) as mock_setup,
+        patch(
+            "custom_components.dynamic_energy_cost.async_setup_entry", return_value=True
+        ) as mock_setup_entry,
+    ):
         # Initialize the flow
         result = await hass.config_entries.flow.async_init(
             DOMAIN, context={"source": config_entries.SOURCE_USER}
